ad13ff364be068801166ca216eadcf19addbc192
Zachary Miller <zach_miller13@icloud.com>
2022-09-25 21:25:39 -0400

    Nearly finished product

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Zachary Miller on 9/25/22.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
---
+++ Today/ReminderDoneButton.swift
@@ -0,0 +1,13 @@
+//
+//  ReminderDoneButton.swift
+//  Today
+//
+//  Created by Zachary Miller on 9/25/22.
+//
+
+import UIKit
+
+class ReminderDoneButton: UIButton {
+    var id: Reminder.ID?
+    
+}
